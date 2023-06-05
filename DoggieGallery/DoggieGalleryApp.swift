256c703ef4e0b50149459b7cd07751bc60237afd
Kevin Velazco <kvela033@fiu.edu>
2023-06-05 13:27:10 -0400

    Initial Commit

---
+++ DoggieGallery/DoggieGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggieGalleryApp.swift
+//  DoggieGallery
+//
+//  Created by Kevin Velazco on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DoggieGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
